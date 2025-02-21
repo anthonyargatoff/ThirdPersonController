d47c316b6f75ebb9447ddb9415f56eb8c1f4e8df
anthonyargatoff <anthonyargatoff@gmail.com>
2025-02-21 10:57:04 -0800

    Double jump

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
   [SerializeField] private float dashSpeed;
   private Rigidbody rb;
   [SerializeField] private CinemachineCamera freeLookCamera;
+  private int jumpStatus = 0;
   void Start()
   {
     inputManager.OnMove.AddListener(MovePlayer);
@@ -24,12 +25,26 @@ public class PlayerController : MonoBehaviour
 
   private void Jump()
   {
+    if (jumpStatus >= 2)
+    {
+      return;
+    }
     rb.AddForce(new Vector3(0, jumpHeight, 0));
+    jumpStatus += 1;
   }
 
   void Update()
   {
     transform.forward = freeLookCamera.transform.forward;
     transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
+  }
+
+  void OnCollisionEnter(Collision collision)
+  {
+    if (collision.gameObject.CompareTag("Ground"))
+    {
+      Debug.Log("Collision with ground");
+      jumpStatus = 0;
+    }
   }
 }
