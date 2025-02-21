becdad2b51586d5cc83eb9f797d4b0d16d9300f1
anthonyargatoff <anthonyargatoff@gmail.com>
2025-02-21 11:47:08 -0800

    Can collect coins

--- Assets/scripts/Coin.cs
+++ Assets/scripts/Coin.cs
@@ -1,12 +1,21 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Coin : MonoBehaviour
 {
 
+  public UnityEvent CollectCoin = new();
   [SerializeField] private float rotationSpeed = 500;
 
   void Update()
   {
     transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+  }
+
+  void OnTriggerEnter(Collider other)
+  {
+    Debug.Log($"{gameObject.name} has been collected");
+    CollectCoin?.Invoke();
+    Destroy(gameObject);
   }
 }
